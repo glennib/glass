c12cec840dc4df7605665a0d23eaea6e4127cc16
Glenn Bitar <glennbitar@gmail.com>
2025-03-29 21:07:36 +0100

    split

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Instant;
 
+use anyhow::bail;
 use axum::Router;
 use axum::body::Body;
 use axum::extract;
@@ -27,10 +28,12 @@ use fast_image_resize::images::Image;
 use image::DynamicImage;
 use image::ImageError;
 use image::ImageReader;
+use image::RgbaImage;
 use ravif::EncodedImage;
 use ravif::Encoder;
 use ravif::Img;
 use tokio::net::TcpListener;
+use tokio::task::spawn_blocking;
 use tower::limit::ConcurrencyLimitLayer;
 use tracing::debug;
 use tracing::error;
@@ -109,8 +112,12 @@ struct Server {
 struct Convert {
     /// Source image
     image: PathBuf,
-    width: u32,
-    height: u32,
+    #[clap(long)]
+    width: Option<u32>,
+    #[clap(long)]
+    height: Option<u32>,
+    #[clap(long)]
+    scale: Option<f64>,
     /// Target file
     output: PathBuf,
 }
@@ -119,6 +126,14 @@ struct Convert {
 struct State {
     config: Arc<Config>,
     images: Arc<PathBuf>,
+}
+
+#[derive(Clone, Debug, Copy)]
+enum ResizeTo {
+    Width(u32),
+    Height(u32),
+    WidthAndHeight(u32, u32),
+    Scale(f64),
 }
 
 fn main() -> anyhow::Result<()> {
@@ -143,7 +158,10 @@ fn main() -> anyhow::Result<()> {
                 .build()?
                 .block_on(async {
                     let app = Router::new()
-                        .route("/images/resized/{width}/{height}/{image}", get(h_resize))
+                        .route("/images/resized/{width}/{height}/{image}", get(h_wh))
+                        .route("/images/resized/width/{width}/{image}", get(h_w))
+                        .route("/images/resized/height/{width}/{image}", get(h_h))
+                        .route("/images/resized/scale/{scale}/{image}", get(h_s))
                         .layer(ConcurrencyLimitLayer::new(concurrency_limit))
                         .with_state(state);
                     let listener = TcpListener::bind(&addr).await?;
@@ -155,72 +173,171 @@ fn main() -> anyhow::Result<()> {
             image,
             width,
             height,
+            scale,
             output,
         }) => {
-            let image = resize(cli.config, &image, width, height)?;
+            let to = match (width, height, scale) {
+                (Some(width), Some(height), None) => ResizeTo::WidthAndHeight(width, height),
+                (Some(width), None, None) => ResizeTo::Height(width),
+                (None, Some(height), None) => ResizeTo::Width(height),
+                (None, None, Some(scale)) => ResizeTo::Scale(scale),
+                _ => {
+                    bail!("provide one or both of width and height, or only scale");
+                }
+            };
+            let image = load_resize_encode(cli.config, &image, to)?;
             let begin = Instant::now();
             fs::write(&output, image.avif_file)?;
             let elapsed = begin.elapsed();
-            info!(elapsed_secs = elapsed.as_secs_f64(), output=%output.display(), "wrote");
+            debug!(elapsed_secs = elapsed.as_secs_f64(), output=%output.display(), "wrote");
         }
     }
 
     Ok(())
 }
 
-async fn h_resize(
+async fn h_wh(
     extract::State(state): extract::State<State>,
-    Path((target_width, target_height, image)): Path<(u32, u32, String)>,
+    Path((width, height, image)): Path<(u32, u32, String)>,
 ) -> Result<Avif, Error> {
     let image = state.images.join(&image);
-    Ok(Avif(
-        tokio::task::spawn_blocking(move || {
-            resize(state.config, &image, target_width, target_height)
-        })
-        .await
-        .unwrap()?,
-    ))
+    load_resize_encode_async(state.config, image, ResizeTo::WidthAndHeight(width, height)).await
 }
 
-#[instrument(skip(config))]
-fn resize(
-    config: impl Borrow<Config> + 'static,
-    image: &path::Path,
-    target_width: u32,
-    target_height: u32,
-) -> Result<EncodedImage, Error> {
-    info!("resizing");
-    let config = config.borrow();
+async fn h_w(
+    extract::State(state): extract::State<State>,
+    Path((width, image)): Path<(u32, String)>,
+) -> Result<Avif, Error> {
+    let image = state.images.join(&image);
+    load_resize_encode_async(state.config, image, ResizeTo::Width(width)).await
+}
+
+async fn h_h(
+    extract::State(state): extract::State<State>,
+    Path((height, image)): Path<(u32, String)>,
+) -> Result<Avif, Error> {
+    let image = state.images.join(&image);
+    load_resize_encode_async(state.config, image, ResizeTo::Height(height)).await
+}
+
+async fn h_s(
+    extract::State(state): extract::State<State>,
+    Path((scale, image)): Path<(f64, String)>,
+) -> Result<Avif, Error> {
+    let image = state.images.join(&image);
+    load_resize_encode_async(state.config, image, ResizeTo::Scale(scale)).await
+}
+
+async fn load_resize_encode_async(
+    config: impl Borrow<Config> + Send + 'static,
+    image: impl AsRef<path::Path> + Send + 'static,
+    to: ResizeTo,
+) -> Result<Avif, Error> {
+    let image = spawn_blocking(move || load_resize_encode(config, image.as_ref(), to))
+        .await
+        .unwrap()?;
+    Ok(Avif(image))
+}
+
+#[instrument(skip_all)]
+fn load(image: &path::Path) -> Result<RgbaImage, Error> {
     let begin = Instant::now();
     let original = ImageReader::open(image)?.decode()?;
-    let original = original.into_rgba8();
-    let original = DynamicImage::from(original);
-    let mut resized = Image::new(target_width, target_height, PixelType::U8x4);
+    let rgba8 = original.to_rgba8();
+    let elapsed = begin.elapsed();
+    debug!(elapsed_secs = elapsed.as_secs_f64(), "loaded image");
+    Ok(rgba8)
+}
+
+fn aspect_ratio(width: u32, height: u32) -> f64 {
+    f64::from(width) / f64::from(height)
+}
+
+#[instrument(skip_all)]
+fn resize(
+    original: impl Into<DynamicImage>,
+    to: ResizeTo,
+    filter_type: FilterType,
+) -> Result<Image<'static>, Error> {
+    let begin = Instant::now();
+    let original = original.into();
+    let (width, height) = match to {
+        ResizeTo::Width(width) => {
+            let ar = aspect_ratio(original.width(), original.height());
+            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+            let height = (f64::from(width) / ar).round() as u32;
+            (width, height)
+        }
+        ResizeTo::Height(height) => {
+            let ar = aspect_ratio(original.width(), original.height());
+            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+            let width = (f64::from(height) * ar).round() as u32;
+            (width, height)
+        }
+        ResizeTo::WidthAndHeight(width, height) => (width, height),
+        ResizeTo::Scale(scale) => {
+            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+            let width = (f64::from(original.width()) * scale).round() as u32;
+            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+            let height = (f64::from(original.height()) * scale).round() as u32;
+            (width, height)
+        }
+    };
+    debug!(width, height);
+    let mut resized = Image::new(width, height, PixelType::U8x4);
     let mut resizer = Resizer::new();
     resizer
         .resize(
             &original,
             &mut resized,
             &ResizeOptions::new().resize_alg(fast_image_resize::ResizeAlg::Convolution(
-                config.filter_type.into(),
+                filter_type.into(),
             )),
         )
-        .unwrap();
-    let rgba: &[rgb::Rgba<u8>] = rgb::bytemuck::cast_slice(resized.buffer());
+        .map_err(|error| Error::FailedToResize {
+            message: error.to_string(),
+        })?;
+    let elapsed = begin.elapsed();
+    debug!(elapsed_secs = elapsed.as_secs_f64(), "resized");
+    Ok(resized)
+}
+
+#[instrument(skip_all)]
+fn encode(image: Image, quality: f32, speed: u8) -> Result<EncodedImage, Error> {
+    let begin = Instant::now();
+    let rgba: &[rgb::Rgba<u8>] = rgb::bytemuck::cast_slice(image.buffer());
     let img = Img::new(
         rgba,
-        usize::try_from(resized.width()).unwrap(),
-        usize::try_from(resized.height()).unwrap(),
+        usize::try_from(image.width()).unwrap(),
+        usize::try_from(image.height()).unwrap(),
     );
-
     let res = Encoder::new()
-        .with_quality(config.quality)
-        .with_speed(config.speed)
+        .with_quality(quality)
+        .with_speed(speed)
         .encode_rgba(img)
         .unwrap();
     let elapsed = begin.elapsed();
-    info!(elapsed_secs = elapsed.as_secs_f64(), "done");
+    let bytes = res.avif_file.len();
+    #[allow(clippy::cast_precision_loss)]
+    let kilobytes = bytes as f64 / 1024.0;
+    debug!(elapsed_secs = elapsed.as_secs_f64(), kilobytes, "encoded image");
     Ok(res)
+}
+
+#[instrument(skip(config))]
+fn load_resize_encode(
+    config: impl Borrow<Config> + 'static,
+    image: &path::Path,
+    to: ResizeTo,
+) -> Result<EncodedImage, Error> {
+    let config = config.borrow();
+    let begin = Instant::now();
+    let original = load(image)?;
+    let resized = resize(original, to, config.filter_type)?;
+    let encoded = encode(resized, config.quality, config.speed)?;
+    let elapsed = begin.elapsed();
+    debug!(elapsed_secs = elapsed.as_secs_f64(), "done");
+    Ok(encoded)
 }
 
 struct Avif(EncodedImage);
